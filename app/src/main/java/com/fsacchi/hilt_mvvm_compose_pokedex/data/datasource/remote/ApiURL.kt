f3c8359419f43be8fae71d8963421b1f7e2cc7da
Felipe Sacchi <felipe.sacchi@gmail.com>
2023-12-04 16:33:40 -0300

    Removendo APi Key

--- app/src/main/java/com/fsacchi/hilt_mvvm_compose_pokedex/data/datasource/remote/ApiURL.kt
+++ app/src/main/java/com/fsacchi/hilt_mvvm_compose_pokedex/data/datasource/remote/ApiURL.kt
@@ -1,7 +1,6 @@
 package com.fsacchi.hilt_mvvm_compose_pokedex.data.datasource.remote
 
 object ApiURL {
-    const val API_KEY = "59cd6896d8432f9c69aed9b86b9c2931"
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val IMAGE_URL = "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/"
     const val IMAGE_URL_TYPE = "https://raw.githubusercontent.com/msikma/pokesprite/master/misc/types/masters/"
